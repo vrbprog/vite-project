4b79c9783ec12a348bda439623ae062825073cbd
vrbprog <vrbdsp@gmail.com>
2024-06-29 12:02:46 +0300

    Reset styles

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
+document.querySelector('#app').innerHTML = 
+`
+  <div class="container">
     <h1>Hello Vite!</h1>
   </div>
 `
-
